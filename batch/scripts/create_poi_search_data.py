31c28b3e2483dc47678753d46dfce45e6b37a8b4
satoshi komatsu <84182110+satoshi7190@users.noreply.github.com>
2025-07-20 12:36:39 +0900

    push

--- batch/scripts/create_poi_search_data.py
+++ batch/scripts/create_poi_search_data.py
@@ -1,6 +1,5 @@
 import glob
 import json
-import math
 from pathlib import Path
 
 import geopandas as gpd
@@ -8,20 +7,12 @@ from shapely.geometry import shape
 from shapely.geometry.base import BaseGeometry
 
 
-def lonlat_to_tile_coords(lon: float, lat: float, zoom: int = 14):
-    """経度・緯度 → タイル座標 (z/x/y) に変換"""
-    lat_rad = math.radians(lat)
-    n = 2.0**zoom
-    x_tile = int((lon + 180.0) / 360.0 * n)
-    y_tile = int(
-        (1.0 - math.log(math.tan(lat_rad) + 1 / math.cos(lat_rad)) / math.pi) / 2.0 * n
-    )
-    return {"x": x_tile, "y": y_tile, "z": zoom}
+
 
 
 OUTPUT_DIR = Path(__file__).resolve().parent.parent.parent / "data"
 
-# TODO:　引数でファイルパスをわたすのがいいかも
+# TODO: 引数でファイルパスをわたすのがいいかも
 search_data_dict = {
     "fac_poi": {
         "name_key": "name",
@@ -85,13 +76,13 @@ def create_search_json(file_paths, output_json=OUTPUT_DIR / "search_data.json"):
             path = config["path"]  # 相対パス等で指定されたファイル名
 
             for idx, row in gdf.iterrows():
-                feature_id = row.name  # ← indexがFeature.idになる
+                feature_id = row["id"]
                 geom = row["geometry"]
                 if geom is not None:
                     shapely_geom = shape(geom)
                     rep_point = get_representative_point(shapely_geom)
 
-                    tile_coords = lonlat_to_tile_coords(*rep_point, zoom=14)
+
 
                     name = convert_nan_to_none(row.get(name_key))
                     search_values = [
@@ -108,7 +99,6 @@ def create_search_json(file_paths, output_json=OUTPUT_DIR / "search_data.json"):
                         "search_values": search_values,
                         "feature_id": feature_id,
                         "point": rep_point,
-                        "tile_coords": tile_coords,
                         "prop_id": prop_id,
                         "path": path,
                     }
